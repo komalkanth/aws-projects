32a5fe29b6110f86d5c6349fe86e85456b873a6b
komalkanth <>
2025-10-16 19:40:11 +0530

    subnet map to refer az-id instead of az-name

--- 3-tier-web/tf-code/main.tf
+++ 3-tier-web/tf-code/main.tf
@@ -9,28 +9,28 @@ module "vpc" {
   vpc_name      = "vpc-1"
   vpc_main_cidr = "10.75.0.0/16"
   public_subnet_cidr_map = {
-    "us-east-1e" : {
+    "az1" : {
       "subnet-1" : ["10.75.2.0/24", "internet-access"]
     }
-    "us-east-1b" : {
+    "az2" : {
       "subnet-1" : ["10.75.0.0/24", "internet-access"]
     }
-    "us-east-1c" : {
+    "az3" : {
       "subnet-1" : ["10.75.1.0/24", "internet-access"]
     }
   }
   private_subnet_cidr_map = {
-    "us-east-1e" : {
+    "az1" : {
       "subnet-1" : ["10.75.5.0/24", "web"]
       "subnet-2" : ["10.75.8.0/24", "app"]
       "subnet-3" : ["10.75.11.0/24", "db"]
     }
-    "us-east-1b" : {
+    "az2" : {
       "subnet-1" : ["10.75.3.0/24", "web"]
       "subnet-2" : ["10.75.6.0/24", "app"]
       "subnet-3" : ["10.75.9.0/24", "db"]
     }
-    "us-east-1c" : {
+    "az3" : {
       "subnet-1" : ["10.75.4.0/24", "web"]
       "subnet-2" : ["10.75.7.0/24", "app"]
       "subnet-3" : ["10.75.10.0/24", "db"]
