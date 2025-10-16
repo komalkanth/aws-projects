fd5f78542264ecae970fb0e609aafe6f6b559691
komalkanth <>
2025-10-16 23:51:39 +0530

    add natgw and eip in 3 public subnets

--- 3-tier-web/modules/vpc/routing.tf
+++ 3-tier-web/modules/vpc/routing.tf
@@ -1,3 +1,6 @@
+# ########################
+# ### INTERNET & NAT GATEWAY
+# ########################
 
 # # Resource to create Internet Gateway
 resource "aws_internet_gateway" "main_igw" {
@@ -8,6 +11,33 @@ resource "aws_internet_gateway" "main_igw" {
       Name = "${var.environment}-${var.region_to_name_map[var.region]}-${replace(var.vpc_name, "-", "")}-igw"
     }
   )
+}
+
+# Resource to create EIP for NAT Gateway, one per public subnet
+resource "aws_eip" "natgw_eip" {
+  for_each = aws_subnet.public_subnet
+  domain   = "vpc"
+
+  tags = merge(
+    var.default_tags, {
+      Name = "${var.environment}-${var.region_to_name_map[var.region]}-eip-natgw-${split("-", each.key)[1]}"
+    }
+  )
+}
+
+# Resource to create NAT Gateway in each public subnet
+resource "aws_nat_gateway" "networking_natgw" {
+  for_each      = aws_subnet.public_subnet
+  allocation_id = aws_eip.natgw_eip[each.key].id
+  subnet_id     = each.value.id
+
+  tags = merge(
+    var.default_tags, {
+      Name = "${var.environment}-${var.region_to_name_map[var.region]}-natgw-${split("-", each.key)[1]}"
+    }
+  )
+
+  depends_on = [aws_internet_gateway.main_igw]
 }
 
 # #########################
