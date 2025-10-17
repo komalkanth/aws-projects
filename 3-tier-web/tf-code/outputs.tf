10cc47d0d3f07cb8e4c23b43f7519582cb394d6b
komalkanth <>
2025-10-17 17:52:54 +0530

    comment all root module outputs

--- 3-tier-web/tf-code/outputs.tf
+++ 3-tier-web/tf-code/outputs.tf
@@ -1,3 +1,15 @@
-# output "private_subnet_name2id_map" {
-#   value = module.vpc.private_subnet_name2id_map
+# output "pvt_az1_rt" {
+#   value = data.aws_route_tables.pvt_az1_rt
+# }
+
+# output "pvt_az2_rt" {
+#   value = data.aws_route_tables.pvt_az2_rt.ids
+# }
+
+# output "pvt_az3_rt" {
+#   value = data.aws_route_tables.pvt_az3_rt.ids
+# }
+
+# output "nat_gateway_ids" {
+#   value = module.vpc.nat_gateway_ids
 # }
